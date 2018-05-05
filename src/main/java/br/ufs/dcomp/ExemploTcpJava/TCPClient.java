cf8264b2f343965930ead6ce52d9d27fa9cf4fbb
Luis Felipe Cruz Santos <luis.santos@dcomp.ufs.br>
2018-05-05 19:08:22 +0000

    Chat Rádio amador feito em Java a partir do código-fonte do Prof. Tarcísio da Rocha

--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPClient.java
@@ -5,6 +5,8 @@ package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 public class TCPClient{
     public static void main(String[] args){
         try {
@@ -14,13 +16,39 @@ public class TCPClient{
             
             InputStream is = sock.getInputStream(); // Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); // Canal de saída de dados
-            String msg = "Olá, DCOMP!!!";
-            byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
+            
+            String msg;
+            String msg2;
+            
+            while(true) {
+                Scanner sc = new Scanner(System.in);
+                msg = sc.nextLine();
+                byte[] buf = msg.getBytes(); // Obtendo a respresntação em bytes da mensagem
 
-            System.out.print("[ Enviando mensagem    ..............................  ");
-            os.write(buf);
-            System.out.println("[OK] ]");
-        }catch(Exception e){System.out.println(e);}    
+                System.out.print("[ Enviando mensagem    ..............................  ");
+                os.write(buf);
+                System.out.println("[OK] ]");
+                buf = null;
+                if (msg.equals("Desligue"))
+                    break;
+            
+                byte[] buf2 = new byte[200];
+                System.out.print("[ Aguardando mensagem   ..............  ");
+                is.read(buf2); // Operação bloqueante (aguardando chegada de dados)
+                System.out.println("[OK] ]");
+            
+                msg2 = new String(buf2); // Mapeando vetor de bytes recebido para String
+            
+                System.out.println("  Servidor: "+ msg2);
+                buf2 = null;
+                if (msg2.equals("Desligue"))
+                    break;
+            } 
+        }
+        
+        catch(Exception e){
+            System.out.println(e);
+        }    
         System.out.println("[ FIM ]");
     }
 }
--- src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
+++ src/main/java/br/ufs/dcomp/ExemploTcpJava/TCPServer.java
@@ -5,6 +5,8 @@ package br.ufs.dcomp.ExemploTcpJava;
 
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 public class TCPServer{
     public static void main(String[] args){
         
@@ -19,16 +21,38 @@ public class TCPServer{
             
             InputStream is = sock.getInputStream(); //Canal de entrada de dados
             OutputStream os = sock.getOutputStream(); //Canal de saída de dados
-            byte[] buf = new byte[20]; // buffer de recepção
+            
+            String msg3;
+            String msg4;
 
-            System.out.print("[ Aguardando recebimento de mensagem   ..............  ");
-            is.read(buf); // Operação bloqueante (aguardando chegada de dados)
-            System.out.println("[OK] ]");
+            while (true) {
+                byte[] buf = new byte[200]; // buffer de recepção
+                System.out.print("[ Aguardando mensagem   ..............  ");
+                is.read(buf); // Operação bloqueante (aguardando chegada de dados)
+                System.out.println("[OK] ]");
             
-            String msg = new String(buf); // Mapeando vetor de bytes recebido para String
+                msg3 = new String(buf); // Mapeando vetor de bytes recebido para String
             
-            System.out.println("  Mensagem recebida: "+ msg);
-        }catch(Exception e){System.out.println(e);}    
+                System.out.println("  Cliente: "+ msg3);
+                buf = null;
+                if (msg3.equals("Desligue"))
+                    break;
+                
+                Scanner sc = new Scanner(System.in);
+                msg4 = sc.nextLine();
+                byte[] buf2 = msg4.getBytes();
+                System.out.print("[ Enviando mensagem    ..............................  ");
+                os.write(buf2);
+                System.out.println("[OK] ]");
+                buf2 = null;
+                if (msg4.equals("Desligue"))
+                    break;
+            } 
+        }
+        
+        catch(Exception e){
+            System.out.println(e);
+        }    
         System.out.println("[ FIM ]");
     }
 }
